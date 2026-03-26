053ee2f1e7c6a886367fd18c96d9d125d8c5f058
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-03-26 13:22:37 +0100

    format

--- Examples/WasmExample/Sources/WebApp/App.swift
+++ Examples/WasmExample/Sources/WebApp/App.swift
@@ -2,8 +2,8 @@ import ElementaryUI
 
 @main
 struct App {
-  static func main() {
-    let app = Application(ContentView())
-    app.mount(in: .body)
-  }
+    static func main() {
+        let app = Application(ContentView())
+        app.mount(in: .body)
+    }
 }
--- Examples/WasmExample/Sources/WebApp/Model.swift
+++ Examples/WasmExample/Sources/WebApp/Model.swift
@@ -48,93 +48,129 @@ extension WiseQuote {
     static let allQuotes: [WiseQuote] = [
         WiseQuote(
             text: "The only way to do great work is to love what you do.",
-            author: "Steve Jobs"),
+            author: "Steve Jobs"
+        ),
         WiseQuote(
             text: "The best way to predict the future is to invent it.",
-            author: "Alan Kay"),
+            author: "Alan Kay"
+        ),
         WiseQuote(
             text: "In the middle of difficulty lies opportunity.",
-            author: "Albert Einstein"),
+            author: "Albert Einstein"
+        ),
         WiseQuote(
             text: "It does not matter how slowly you go as long as you do not stop.",
-            author: "Confucius"),
+            author: "Confucius"
+        ),
         WiseQuote(
             text:
                 "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
-            author: "Ralph Waldo Emerson"),
+            author: "Ralph Waldo Emerson"
+        ),
         WiseQuote(
-            text: "The only true wisdom is in knowing you know nothing.", author: "Socrates"),
+            text: "The only true wisdom is in knowing you know nothing.",
+            author: "Socrates"
+        ),
         WiseQuote(
             text: "Life is what happens when you're busy making other plans.",
-            author: "John Lennon"),
+            author: "John Lennon"
+        ),
         WiseQuote(text: "The mind is everything. What you think you become.", author: "Buddha"),
         WiseQuote(
             text: "Strive not to be a success, but rather to be of value.",
-            author: "Albert Einstein"),
+            author: "Albert Einstein"
+        ),
         WiseQuote(
             text: "Your time is limited, don't waste it living someone else's life.",
-            author: "Steve Jobs"),
+            author: "Steve Jobs"
+        ),
         WiseQuote(
             text: "If you want to lift yourself up, lift up someone else.",
-            author: "Booker T. Washington"),
+            author: "Booker T. Washington"
+        ),
         WiseQuote(
-            text: "The journey of a thousand miles begins with one step.", author: "Lao Tzu"),
+            text: "The journey of a thousand miles begins with one step.",
+            author: "Lao Tzu"
+        ),
         WiseQuote(
-            text: "Be the change that you wish to see in the world.", author: "Mahatma Gandhi"),
+            text: "Be the change that you wish to see in the world.",
+            author: "Mahatma Gandhi"
+        ),
         WiseQuote(
             text: "I have not failed. I've just found 10,000 ways that won't work.",
-            author: "Thomas Edison"),
+            author: "Thomas Edison"
+        ),
         WiseQuote(
             text:
                 "The best time to plant a tree was 20 years ago. The second best time is now.",
-            author: "Chinese Proverb"),
+            author: "Chinese Proverb"
+        ),
         WiseQuote(
             text: "An investment in knowledge pays the best interest.",
-            author: "Benjamin Franklin"),
+            author: "Benjamin Franklin"
+        ),
         WiseQuote(
             text:
                 "Education is the most powerful weapon which you can use to change the world.",
-            author: "Nelson Mandela"),
+            author: "Nelson Mandela"
+        ),
         WiseQuote(
-            text: "Simplicity is the ultimate sophistication.", author: "Leonardo da Vinci"),
+            text: "Simplicity is the ultimate sophistication.",
+            author: "Leonardo da Vinci"
+        ),
         WiseQuote(text: "Talk is cheap. Show me the code.", author: "Linus Torvalds"),
         WiseQuote(
-            text: "Given enough eyeballs, all bugs are shallow.", author: "Eric S. Raymond"),
+            text: "Given enough eyeballs, all bugs are shallow.",
+            author: "Eric S. Raymond"
+        ),
         WiseQuote(
             text:
                 "The most dangerous phrase in the language is: We've always done it this way.",
-            author: "Grace Hopper"),
+            author: "Grace Hopper"
+        ),
         WiseQuote(
-            text: "Premature optimization is the root of all evil.", author: "Donald Knuth"),
+            text: "Premature optimization is the root of all evil.",
+            author: "Donald Knuth"
+        ),
         WiseQuote(
             text:
                 "Programs must be written for people to read, and only incidentally for machines to execute.",
-            author: "Harold Abelson"),
+            author: "Harold Abelson"
+        ),
         WiseQuote(
             text:
                 "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-            author: "Martin Fowler"),
+            author: "Martin Fowler"
+        ),
         WiseQuote(
-            text: "Simplicity is prerequisite for reliability.", author: "Edsger W. Dijkstra"),
+            text: "Simplicity is prerequisite for reliability.",
+            author: "Edsger W. Dijkstra"
+        ),
         WiseQuote(
             text: "One of my most productive days was throwing away 1,000 lines of code.",
-            author: "Ken Thompson"),
+            author: "Ken Thompson"
+        ),
         WiseQuote(
             text:
                 "There are only two kinds of languages: the ones people complain about and the ones nobody uses.",
-            author: "Bjarne Stroustrup"),
+            author: "Bjarne Stroustrup"
+        ),
         WiseQuote(
             text: "Code is like humor. When you have to explain it, it's bad.",
-            author: "Cory House"),
+            author: "Cory House"
+        ),
         WiseQuote(
             text: "Before software can be reusable it first has to be usable.",
-            author: "Ralph Johnson"),
+            author: "Ralph Johnson"
+        ),
         WiseQuote(text: "Make it work, make it right, make it fast.", author: "Kent Beck"),
         WiseQuote(
             text: "The function of good software is to make the complex appear to be simple.",
-            author: "Grady Booch"),
+            author: "Grady Booch"
+        ),
         WiseQuote(
             text: "In real open source, you have the right to control your own destiny.",
-            author: "Linus Torvalds"),
+            author: "Linus Torvalds"
+        ),
     ]
 }
